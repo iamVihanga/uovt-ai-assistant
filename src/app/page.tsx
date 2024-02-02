0ea558fef8727dfbfaf1abb368ec92dde60e5a80
Vihanga Rashan Silva <vihangarashansilva@gmail.com>
2024-02-02 11:38:33 +0530

    Index page change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import Avatar from "@/components/Avatar";
 import ChatUI from "@/components/ChatUI";
 
 export default function Home() {
   return (
-    <ChatUI />
+    <div className="w-full flex items-center bg-[#000]">
+      <div className="flex-1 h-full flex items-center justify-center">
+        {/* <div className="">
+          <h2 className='font-bold  text-sm text-cyan-500'><span className='text-green-500 font-medium'>Hello</span> UOVT</h2>
+          <p className='text-[10px] text-gray-200 -mt-0.5'>University AI Assistant</p>
+        </div> */}
+        <Avatar isOnline dot userType='ai' />
+        <h1 className="ml-5 text-white text-5xl font-black">UOVT AI ChatBot</h1>
+      </div>
+      <div className="w-[400px]">
+        <ChatUI />
+      </div>
+    </div>
   );
 }
