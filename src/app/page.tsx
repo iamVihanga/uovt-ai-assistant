837801310144e3ade2e01e7ece17456c2190a22b
Vihanga Rashan Silva <vihangarashansilva@gmail.com>
2024-02-02 12:43:20 +0530

    Design 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,14 @@ import ChatUI from "@/components/ChatUI";
 
 export default function Home() {
   return (
-    <div className="w-full flex items-center bg-[#000]">
-      <div className="flex-1 h-full flex items-center justify-center">
-        {/* <div className="">
-          <h2 className='font-bold  text-sm text-cyan-500'><span className='text-green-500 font-medium'>Hello</span> UOVT</h2>
-          <p className='text-[10px] text-gray-200 -mt-0.5'>University AI Assistant</p>
-        </div> */}
-        <Avatar isOnline dot userType='ai' />
-        <h1 className="ml-5 text-white text-5xl font-black">UOVT AI ChatBot</h1>
+    <div className="flex items-center h-screen max-h-screen overflow-hidden bg">
+      <div className="flex-1 max-h-screen relative">
+        <div className="absolute top-0 left-0 flex items-center justify-center w-full h-full">
+          <img src="poster1.png" className="w-[680px] h-[650px]" />
+        </div>
       </div>
-      <div className="w-[400px]">
+
+      <div className="max-h-screen w-[700px] overflow-scroll">
         <ChatUI />
       </div>
     </div>
