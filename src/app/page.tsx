d9014c1200e6ca291d6dd491112806dbe55e5023
Vihanga Rashan Silva <vihangarashansilva@gmail.com>
2024-02-02 12:52:33 +0530

    Design 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,11 @@ import ChatUI from "@/components/ChatUI";
 export default function Home() {
   return (
     <div className="flex items-center h-screen max-h-screen overflow-hidden bg">
-      <div className="flex-1 max-h-screen relative">
-        <div className="absolute top-0 left-0 flex items-center justify-center w-full h-full">
-          <img src="poster1.png" className="w-[680px] h-[650px]" />
-        </div>
+      <div className="flex-1 max-h-screen ">
+        <img src="poster1.png" className="w-full h-full" />
       </div>
 
-      <div className="max-h-screen w-[700px] overflow-scroll">
+      <div className="max-h-screen w-[600px] overflow-scroll">
         <ChatUI />
       </div>
     </div>
